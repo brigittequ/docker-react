f35b2777124a8f1515c04e31edee7b33b596b5bf
brigitte1234 <33143166+brigitte1234@users.noreply.github.com>
2020-07-28 15:06:36 +0800

    initial commit

--- src/App - Copy.js
+++ src/App - Copy.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React say hello!
+          Learn React
         </a>
       </header>
     </div>
